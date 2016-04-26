3003d0e92a5a51a2ccf4591c176813ecdca0e801
Your Name <you@example.com>
2016-04-26 22:53:23 +0000

    Crappy PID Regulator RPi3 Binaries

---
+++ boneyard/lang/swig/CMakeFiles/_pynifalcon.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "libnifalcon_swigPYTHON_wrap.cxx"
+  "libnifalcon_swig.py"
+  "libnifalcon_swigPYTHON_wrap.cxx"
+  "libnifalcon_swig.py"
+  "CMakeFiles/_pynifalcon.dir/libnifalcon_swigPYTHON_wrap.o"
+  "CMakeFiles/_pynifalcon.dir/home/pi/libnifalcon/lang/src/FalconDeviceBridge.o"
+  "_pynifalcon.pdb"
+  "_pynifalcon.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_pynifalcon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- python_example.py
+++ python_example.py
@@ -25,34 +25,86 @@ pos = np.array(cpos)
 ## Buttons
 # Change gradient / acceleration
 
-while True:
-	run_io()
+import time
+
+cur_time = time.time()
+
+history = []
+
+phase = 0
+
+while True:	
+	# Save Time Delta
+	last_time = cur_time
+	cur_time = time.time()
+	delta_time = cur_time - last_time
 	
 	#Save Last Position
 	last_pos = pos
-	
 	# Update Position
 	lib.get_position(cpos)
 	pos = np.array(cpos)
 	
-	# Compute Normal Vector
-	direction = pos / np.sum(pos)
+	delta_pos = pos - last_pos
 	
 	# Calculate Differential
-	gradient = pos - last_pos
-	#force = abs(pos) * -direction - gradient
+	gradient = delta_pos / delta_time
 	
-	damping = 1
-	force = gradient * damping # Acceleration, Damping Friction
+	# Compute Normal Vector
+	center_distance = np.sqrt(np.dot(pos, pos))
+	normal_vec = pos / center_distance
+	
+		
+	# PID Regulator
+	P = 60.0 * 5
+	D = -1.9 * 10
+	I = 30.5
+	phase += 2 * np.pi * delta_time
+	
+	
+	dildo_gravity = np.array([0, 1, 0]) * 9.81 * .4
+	dildo_sinus = np.array([0, 1, 0])* 1000.0
+	target_position = np.sin(dildo_sinus * phase * 20)
+	target_position = np.zeros(3)
+	
+	target_position[1] = 0.05
+	target_position[1] = 0.05 * np.sin(phase / 10.71) * 0.3 + 0.01
+	#target_position = [0, np.sin(phase) * 0.0011, 0]
+	
+	difference = target_position - pos
+	
+	### Force Feedback
+	
+	#force = normal_vec * 1.21 * P + gradient * np.log(gradient) / np.log(8) * 0.1 * D # * P - gradient * D#- gradient
+	
+	#difference_gradient = difference / delta_time
+	#force = difference * P + difference_gradient * D
+	
+	force = dildo_gravity + difference * P + difference / delta_time * D + difference * delta_time * I
+	print type(pos), type(difference), type(force)
+	force *= 0.001
+	
+	lib.set_force(cVec3(force[0], force[1], force[2]))
+
+	
+	## Archive / PID stuff
+	
+	#from math import sin
+	#force[2] = sin(cnt / 8.) * 0.01
+	#force[0] += sin(cnt/16) * 0.002
+	
+	damping = 0.01
+	#force = gradient * damping # Acceleration, Damping Friction
 	
 	#force += abs(pos) * -direction * .1
 	
 	#force[0] = np.sin(cnt / 12) * 10
 		
-	lib.set_force(cVec3(force[0], force[1], force[2]))
+
+	run_io()
 	
 	# Log Limiting Counter
 	cnt += 1
 	if cnt % 40 == 0: # 25 Hz Update Rate
-		print pos[0], pos[1], pos[2],
-		print "force", force[0]
+		print "Distance", center_distance
+		print "Delta Time", delta_time, "Force Vector", force
